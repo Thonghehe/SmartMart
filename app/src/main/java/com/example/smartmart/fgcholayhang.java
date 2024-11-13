7becb8ed9c7594d0686e8e99bd231d17bfefefe5
Nk-Thong <thongk21@gmail.com>
2024-11-13 20:12:07 +0700

    update giao dien 20/11

---
+++ app/src/main/java/com/example/smartmart/fgcholayhang.java
@@ -0,0 +1,21 @@
+package com.example.smartmart;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class fgcholayhang extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate layout cho Fragment 1
+        View view=inflater.inflate(R.layout.fragment_fgcholayhang, container, false);
+        return view;
+    }
+    }
