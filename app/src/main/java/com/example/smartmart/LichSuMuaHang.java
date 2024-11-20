36778edd48d9c6bee0168139a5f1cc39693d4d80
paickle77 <levanan.8b@gmail.com>
2024-11-20 08:17:06 +0700

    update giao dien 20/11

---
+++ app/src/main/java/com/example/smartmart/LichSuMuaHang.java
@@ -0,0 +1,54 @@
+package com.example.smartmart;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class LichSuMuaHang extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_lichsumuahang);
+
+        Button btnFragment1 = findViewById(R.id.btndalayhang);
+        Button btnFragment2 = findViewById(R.id.btnchogiaohang);
+
+        // Hiển thị fgdalayhang làm Fragment mặc định khi mở Activity
+        if (savedInstanceState == null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragmentContainer, new fgdalayhang());
+            fragmentTransaction.commit();
+        }
+
+        // Xử lý sự kiện nhấn nút để chuyển Fragment
+        btnFragment1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer, new fgdalayhang());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        btnFragment2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer, new fgcholayhang());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/smartmart/fgcholayhang.java
@@ -0,0 +1,20 @@
+package com.example.smartmart;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+public class fgcholayhang extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate layout cho Fragment 1
+        View view=inflater.inflate(R.layout.fragment_fgcholayhang, container, false);
+        return view;
+    }
+    }
